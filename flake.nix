b2ad0ee86ed9261494ff53afa21f136cae5deb8a
Qazm <17500283+qazmlp@users.noreply.github.com>
2025-06-01 12:16:06 +0000

    Add initial Nix flake configuration for package installation

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A simple Nix flake to install packages in your profile";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # List your desired packages here
+        myPackages = with pkgs; [
+          git
+          curl
+          htop
+          vscode
+          vivaldi
+          keepassxc
+          syncthing
+          syncthingtray
+          element-desktop
+          flameshot
+          signal-desktop
+          steam
+          lutris
+          heroic
+          prismlauncher
+          mangohud
+          gamemode
+          bottles
+          discord
+          teamspeak_client
+          obs-studio
+          vkbasalt
+          obsidian
+          ghostwriter
+          xournalpp
+          focuswriter
+        ];
+      in {
+        packages.default = pkgs.buildEnv {
+          name = "my-profile";
+          paths = myPackages;
+        };
+      }
+    );
+}
